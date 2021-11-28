3649008a6218236a7cd056e36965fedded13db41
Deng <donggdeng@gmail.com>
2021-11-28 17:48:25 +0800

    Finish

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from "recoil"
+
+export const playlistIdState = atom({
+    key: "playlistIdState",
+    default: '6nOTx7yrY7edhpU5GLQBPi'
+})
+
+export const playlistState = atom({
+    key: "playlistState",
+    default: null,
+})
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,23 +3,38 @@ import {
     SearchIcon,
     LibraryIcon,
     PlusCircleIcon,
-    HeartIcon,
     RssIcon
-} from "@heroicons/react/outline"
+} from "@heroicons/react/outline";
+
+import {
+    HeartIcon,
+} from "@heroicons/react/solid";
+
 import { signOut, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { playlistIdState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
 
 function Sidebar() {
+    const spotifyApi = useSpotify();
     const { data: session, status } = useSession();
-    console.log(session);
+    const [playlists, setPlaylists ] = useState([]);
+    const [playlistId, setPlaylistId] = useRecoilState(playlistIdState);
+
+    console.log(playlistId)
+
+    useEffect(() => {
+        if (spotifyApi.getAccessToken()) {
+            spotifyApi.getUserPlaylists().then((data) => {
+                setPlaylists(data.body.items);
+            })
+        }
+    }, [session, spotifyApi]);
 
     return (
-        <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
-            <div className="space-y-8">
-                <button className="flex items-center space-x-2 hover:text-white"
-                        onClick={() => signOut()}>
-                    <HomeIcon className="h-5 w-5" />
-                    <p>Log out</p>
-                </button>
+        <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex pb-36">
+            <div className="space-y-4">
                 <button className="flex items-center space-x-2 hover:text-white">
                     <HomeIcon className="h-5 w-5" />
                     <p>Home</p>
@@ -39,24 +54,19 @@ function Sidebar() {
                     <p>Create Playlist</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <HeartIcon className="h-5 w-5" />
+                    <HeartIcon className="h-5 w-5 text-blue-500" />
                     <p>Like Songs</p>
                 </button>
                 <button className="flex items-center space-x-2 hover:text-white">
-                    <RssIcon className="h-5 w-5"/>
+                    <RssIcon className="h-5 w-5 text-green-500"/>
                     <p>Your episodes </p>
                 </button>
                 <hr className="border-t-[0.1px] border-gray-900"/>
 
-                {/* Playlist */}
+                {playlists.map((playlist) => (
+                    <p key={playlist.id} onClick={() => {setPlaylistId(playlist.id)}} className="cursor-pointer hover:text-white">{playlist.name}</p>
 
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
-                <p className="cursor-pointer hover:text-white">Playlist name...</p>
+                ))}
             </div>
         </div>
     )
